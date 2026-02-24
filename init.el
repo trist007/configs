e342309787987fd8dd7df17d48db4139a2c698e3
Tristan <trist007@darkterminal.net>
2026-02-23 22:57:58 -0600

    emacs config

---
+++ init.el
@@ -0,0 +1,526 @@
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
+(setq package-check-signature nil)
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+; This is my super-poopy .emacs file.
+; I barely know how to program LISP, and I know
+; even less about ELISP.  So take everything in
+; this file with a grain of salt!
+;
+; - Casey
+
+; Stop Emacs from losing undo information by
+; setting very high limits for undo buffers
+(setq undo-limit 20000000)
+(setq undo-strong-limit 40000000)
+
+; Determine the underlying operating system
+(setq casey-aquamacs (featurep 'aquamacs))
+(setq casey-linux (featurep 'x))
+(setq casey-win32 (not (or casey-aquamacs casey-linux)))
+
+(setq casey-todo-file "w:/handmade/code/todo.txt")
+(setq casey-log-file "w:/handmade/code/log.txt")
+
+(global-hl-line-mode 1)
+(set-face-background 'hl-line "midnight blue")
+
+(setq compilation-directory-locked nil)
+
+(setq shift-select-mode nil)
+(setq enable-local-variables nil)
+(setq casey-font "outline-DejaVu Sans Mono")
+
+(when casey-win32 
+  (setq casey-makescript "build.bat")
+  (setq casey-font "outline-Liberation Mono")
+)
+
+(when casey-aquamacs 
+  (cua-mode 0) 
+  (osx-key-mode 0)
+
+  (setq mac-command-modifier 'meta)
+  (setq x-select-enable-clipboard t)
+  (setq aquamacs-save-options-on-quit 0)
+  (setq special-display-regexps nil)
+  (setq special-display-buffer-names nil)
+
+  (setq mac-command-key-is-meta t)
+  (scroll-bar-mode nil)
+  (setq mac-pass-command-to-system nil)
+  (setq casey-makescript "./build.macosx")
+)
+
+(when casey-linux
+  (setq casey-makescript "./build.linux")
+  (display-battery-mode 1)
+)
+
+; Turn off the toolbar
+(tool-bar-mode 0)
+
+(load-library "view")
+(require 'cc-mode)
+(require 'ido)
+(require 'compile)
+(ido-mode t)
+
+(defun run-raddbg ()
+  (interactive)
+  (shell-command "raddbg ..\..\build\plata.exe &"))
+
+(global-set-key (kbd "F3") 'run-raddbg)
+
+(defun casey-ediff-setup-windows (buffer-A buffer-B buffer-C control-buffer)
+  (ediff-setup-windows-plain buffer-A buffer-B buffer-C control-buffer)
+)
+(setq ediff-window-setup-function 'casey-ediff-setup-windows)
+(setq ediff-split-window-function 'split-window-horizontally)
+
+; Turn off the bell on Mac OS X
+(defun nil-bell ())
+(setq ring-bell-function 'nil-bell)
+
+; Setup my compilation mode
+(defun casey-big-fun-compilation-hook ()
+  (make-local-variable 'truncate-lines)
+  (setq truncate-lines nil)
+)
+
+(add-hook 'compilation-mode-hook 'casey-big-fun-compilation-hook)
+
+(defun load-todo ()
+  (interactive)
+  (find-file casey-todo-file)
+)
+(define-key global-map "\et" 'load-todo)
+
+(defun insert-timeofday ()
+   (interactive "*")
+   (insert (format-time-string "---------------- %a, %d %b %y: %I:%M%p")))
+(defun load-log ()
+  (interactive)
+  (find-file casey-log-file)
+  (if (boundp 'longlines-mode) ()
+    (longlines-mode 1)
+    (longlines-show-hard-newlines))
+  (if (equal longlines-mode t) ()
+    (longlines-mode 1)
+    (longlines-show-hard-newlines))
+  (end-of-buffer)
+  (newline-and-indent)
+  (insert-timeofday)
+  (newline-and-indent)
+  (newline-and-indent)
+  (end-of-buffer)
+)
+(define-key global-map "\eT" 'load-log)
+
+; no screwing with my middle mouse button
+(global-unset-key [mouse-2])
+
+; Bright-red TODOs
+ (setq fixme-modes '(c++-mode c-mode emacs-lisp-mode))
+ (make-face 'font-lock-fixme-face)
+ (make-face 'font-lock-note-face)
+ (mapc (lambda (mode)
+	 (font-lock-add-keywords
+	  mode
+	  '(("\\<\\(TODO\\)" 1 'font-lock-fixme-face t)
+            ("\\<\\(NOTE\\)" 1 'font-lock-note-face t))))
+	fixme-modes)
+ (modify-face 'font-lock-fixme-face "Red" nil nil t nil t nil nil)
+ (modify-face 'font-lock-note-face "Dark Green" nil nil t nil t nil nil)
+
+; Accepted file extensions and their appropriate modes
+(setq auto-mode-alist
+      (append
+       '(("\\.cpp$"    . c++-mode)
+         ("\\.hin$"    . c++-mode)
+         ("\\.cin$"    . c++-mode)
+         ("\\.inl$"    . c++-mode)
+         ("\\.rdc$"    . c++-mode)
+         ("\\.h$"    . c++-mode)
+         ("\\.c$"   . c++-mode)
+         ("\\.cc$"   . c++-mode)
+         ("\\.c8$"   . c++-mode)
+         ("\\.txt$" . indented-text-mode)
+         ("\\.emacs$" . emacs-lisp-mode)
+         ("\\.gen$" . gen-mode)
+         ("\\.ms$" . fundamental-mode)
+         ("\\.m$" . objc-mode)
+         ("\\.mm$" . objc-mode)
+         ) auto-mode-alist))
+
+; C++ indentation style
+(defconst casey-big-fun-c-style
+  '((c-electric-pound-behavior   . nil)
+    (c-tab-always-indent         . t)
+    (c-comment-only-line-offset  . 0)
+    (c-hanging-braces-alist      . ((class-open)
+                                    (class-close)
+                                    (defun-open)
+                                    (defun-close)
+                                    (inline-open)
+                                    (inline-close)
+                                    (brace-list-open)
+                                    (brace-list-close)
+                                    (brace-list-intro)
+                                    (brace-list-entry)
+                                    (block-open)
+                                    (block-close)
+                                    (substatement-open)
+                                    (statement-case-open)
+                                    (class-open)))
+    (c-hanging-colons-alist      . ((inher-intro)
+                                    (case-label)
+                                    (label)
+                                    (access-label)
+                                    (access-key)
+                                    (member-init-intro)))
+    (c-cleanup-list              . (scope-operator
+                                    list-close-comma
+                                    defun-close-semi))
+    (c-offsets-alist             . ((arglist-close         .  c-lineup-arglist)
+                                    (label                 . -4)
+                                    (access-label          . -4)
+                                    (substatement-open     .  0)
+                                    (statement-case-intro  .  4)
+                                    (statement-block-intro .  c-lineup-for)
+                                    (case-label            .  4)
+                                    (block-open            .  0)
+                                    (inline-open           .  0)
+                                    (topmost-intro-cont    .  0)
+                                    (knr-argdecl-intro     . -4)
+                                    (brace-list-open       .  0)
+                                    (brace-list-intro      .  4)))
+    (c-echo-syntactic-information-p . t))
+    "Casey's Big Fun C++ Style")
+
+
+; CC++ mode handling
+(defun casey-big-fun-c-hook ()
+  ; Set my style for the current buffer
+  (c-add-style "BigFun" casey-big-fun-c-style t)
+  
+  ; 4-space tabs
+  (setq tab-width 4
+        indent-tabs-mode nil)
+
+  ; Additional style stuff
+  (c-set-offset 'member-init-intro '++)
+
+  ; No hungry backspace
+  (c-toggle-auto-hungry-state -1)
+
+  ; Newline indents, semi-colon doesn't
+
+  (setq c-hanging-semi&comma-criteria '((lambda () 'stop)))
+
+  ; Handle super-tabbify (TAB completes, shift-TAB actually tabs)
+  (setq dabbrev-case-replace t)
+  (setq dabbrev-case-fold-search t)
+  (setq dabbrev-upcase-means-case-search t)
+
+  ; Abbrevation expansion
+  (abbrev-mode 1)
+ 
+  (defun casey-header-format ()
+     "Format the given file as a header file."
+     (interactive)
+     (setq BaseFileName (file-name-sans-extension (file-name-nondirectory buffer-file-name)))
+     (insert "#if !defined(")
+     (push-mark)
+     (insert BaseFileName)
+     (upcase-region (mark) (point))
+     (pop-mark)
+     (insert "_H)\n")
+     (insert "/* ========================================================================\n")
+     (insert "   $File: $\n")
+     (insert "   $Date: $\n")
+     (insert "   $Revision: $\n")
+     (insert "   $Creator: Casey Muratori $\n")
+     (insert "   $Notice: (C) Copyright 2014 by Molly Rocket, Inc. All Rights Reserved. $\n")
+     (insert "   ======================================================================== */\n")
+     (insert "\n")
+     (insert "#define ")
+     (push-mark)
+     (insert BaseFileName)
+     (upcase-region (mark) (point))
+     (pop-mark)
+     (insert "_H\n")
+     (insert "#endif")
+  )
+
+  (defun casey-source-format ()
+     "Format the given file as a source file."
+     (interactive)
+     (setq BaseFileName (file-name-sans-extension (file-name-nondirectory buffer-file-name)))
+     (insert "/* ========================================================================\n")
+     (insert "   $File: $\n")
+     (insert "   $Date: $\n")
+     (insert "   $Revision: $\n")
+     (insert "   $Creator: Casey Muratori $\n")
+     (insert "   $Notice: (C) Copyright 2014 by Molly Rocket, Inc. All Rights Reserved. $\n")
+     (insert "   ======================================================================== */\n")
+  )
+
+  (cond ((file-exists-p buffer-file-name) t)
+        ((string-match "[.]hin" buffer-file-name) (casey-source-format))
+        ((string-match "[.]cin" buffer-file-name) (casey-source-format))
+        ((string-match "[.]h" buffer-file-name) (casey-header-format))
+        ((string-match "[.]cpp" buffer-file-name) (casey-source-format)))
+
+  (defun casey-find-corresponding-file ()
+    "Find the file that corresponds to this one."
+    (interactive)
+    (setq CorrespondingFileName nil)
+    (setq BaseFileName (file-name-sans-extension buffer-file-name))
+    (if (string-match "\\.c" buffer-file-name)
+       (setq CorrespondingFileName (concat BaseFileName ".h")))
+    (if (string-match "\\.h" buffer-file-name)
+       (if (file-exists-p (concat BaseFileName ".c")) (setq CorrespondingFileName (concat BaseFileName ".c"))
+	   (setq CorrespondingFileName (concat BaseFileName ".cpp"))))
+    (if (string-match "\\.hin" buffer-file-name)
+       (setq CorrespondingFileName (concat BaseFileName ".cin")))
+    (if (string-match "\\.cin" buffer-file-name)
+       (setq CorrespondingFileName (concat BaseFileName ".hin")))
+    (if (string-match "\\.cpp" buffer-file-name)
+       (setq CorrespondingFileName (concat BaseFileName ".h")))
+    (if CorrespondingFileName (find-file CorrespondingFileName)
+       (error "Unable to find a corresponding file")))
+  (defun casey-find-corresponding-file-other-window ()
+    "Find the file that corresponds to this one."
+    (interactive)
+    (find-file-other-window buffer-file-name)
+    (casey-find-corresponding-file)
+    (other-window -1))
+
+  ; devenv.com error parsing
+  (add-to-list 'compilation-error-regexp-alist 'casey-devenv)
+  (add-to-list 'compilation-error-regexp-alist-alist '(casey-devenv
+   "*\\([0-9]+>\\)?\\(\\(?:[a-zA-Z]:\\)?[^:(\t\n]+\\)(\\([0-9]+\\)) : \\(?:see declaration\\|\\(?:warnin\\(g\\)\\|[a-z ]+\\) C[0-9]+:\\)"
+    2 3 nil (4)))
+)
+
+(defun casey-replace-string (FromString ToString)
+  "Replace a string without moving point."
+  (interactive "sReplace: \nsReplace: %s  With: ")
+  (save-excursion
+    (replace-string FromString ToString)
+  ))
+
+(add-hook 'c-mode-common-hook 'casey-big-fun-c-hook)
+
+(defun casey-save-buffer ()
+  "Save the buffer after untabifying it."
+  (interactive)
+  (save-excursion
+    (save-restriction
+      (widen)
+      (untabify (point-min) (point-max))))
+  (save-buffer))
+
+; TXT mode handling
+(defun casey-big-fun-text-hook ()
+  ; 4-space tabs
+  (setq tab-width 4
+        indent-tabs-mode nil)
+
+  ; Newline indents, semi-colon doesn't
+
+  ; Prevent overriding of alt-s
+  )
+(add-hook 'text-mode-hook 'casey-big-fun-text-hook)
+
+; Window Commands
+(defun w32-restore-frame ()
+    "Restore a minimized frame"
+     (interactive)
+     (w32-send-sys-command 61728))
+
+(defun maximize-frame ()
+    "Maximize the current frame"
+     (interactive)
+     (when casey-aquamacs (aquamacs-toggle-full-frame))
+     (when casey-win32 (w32-send-sys-command 61488)))
+
+; Navigation
+(defun previous-blank-line ()
+  "Moves to the previous line containing nothing but whitespace."
+  (interactive)
+  (search-backward-regexp "^[ \t]*\n")
+)
+
+(defun next-blank-line ()
+  "Moves to the next line containing nothing but whitespace."
+  (interactive)
+  (forward-line)
+  (search-forward-regexp "^[ \t]*\n")
+  (forward-line -1)
+)
+
+
+; ALT-alternatives
+(defadvice set-mark-command (after no-bloody-t-m-m activate)
+  "Prevent consecutive marks activating bloody `transient-mark-mode'."
+  (if transient-mark-mode (setq transient-mark-mode nil)))
+
+(defadvice mouse-set-region-1 (after no-bloody-t-m-m activate)
+  "Prevent mouse commands activating bloody `transient-mark-mode'."
+  (if transient-mark-mode (setq transient-mark-mode nil))) 
+
+(defun append-as-kill ()
+  "Performs copy-region-as-kill as an append."
+  (interactive)
+  (append-next-kill) 
+  (copy-region-as-kill (mark) (point))
+)
+
+(defun casey-replace-in-region (old-word new-word)
+  "Perform a replace-string in the current region."
+  (interactive "sReplace: \nsReplace: %s  With: ")
+  (save-excursion (save-restriction
+		    (narrow-to-region (mark) (point))
+		    (beginning-of-buffer)
+		    (replace-string old-word new-word)
+		    ))
+  )
+
+; Compilation
+(setq compilation-context-lines 0)
+(setq compilation-error-regexp-alist
+    (cons '("^\\([0-9]+>\\)?\\(\\(?:[a-zA-Z]:\\)?[^:(\t\n]+\\)(\\([0-9]+\\)) : \\(?:fatal error\\|warnin\\(g\\)\\) C[0-9]+:" 2 3 nil (4))
+     compilation-error-regexp-alist))
+
+(defun find-project-directory-recursive ()
+  "Recursively search for a makefile."
+  (interactive)
+  (if (file-exists-p casey-makescript) t
+      (cd "../")
+      (find-project-directory-recursive)))
+
+(defun lock-compilation-directory ()
+  "The compilation process should NOT hunt for a makefile"
+  (interactive)
+  (setq compilation-directory-locked t)
+  (message "Compilation directory is locked."))
+
+(defun unlock-compilation-directory ()
+  "The compilation process SHOULD hunt for a makefile"
+  (interactive)
+  (setq compilation-directory-locked nil)
+  (message "Compilation directory is roaming."))
+
+(defun find-project-directory ()
+  "Find the project directory."
+  (interactive)
+  (setq find-project-from-directory default-directory)
+  (switch-to-buffer-other-window "*compilation*")
+  (if compilation-directory-locked (cd last-compilation-directory)
+  (cd find-project-from-directory)
+  (find-project-directory-recursive)
+  (setq last-compilation-directory default-directory)))
+
+(defun make-without-asking ()
+  "Make the current build."
+  (interactive)
+  (if (find-project-directory) (compile casey-makescript))
+  (other-window 1))
+(define-key global-map "\em" 'make-without-asking)
+
+; Commands
+(set-variable 'grep-command "grep -irHn ")
+(when casey-win32
+    (set-variable 'grep-command "findstr -s -n -i -l "))
+
+; Smooth scroll
+(setq scroll-step 3)
+
+; Clock
+(display-time)
+
+; Startup windowing
+(setq next-line-add-newlines nil)
+(setq-default truncate-lines t)
+(setq truncate-partial-width-windows nil)
+
+(defun my-safe-horizontal-split ()
+  "Split window horizontally if no split exists."
+  (interactive)
+  (when (= (length (window-list)) 1)
+    (split-window-horizontally)))
+
+;; Replace your current split-window-horizontally call with this
+(my-safe-horizontal-split)
+;;(split-window-horizontally)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-save-default nil)
+ '(auto-save-interval 0)
+ '(auto-save-list-file-prefix nil)
+ '(auto-save-timeout 0)
+ '(auto-show-mode t t)
+ '(delete-auto-save-files nil)
+ '(delete-old-versions (quote other))
+ '(imenu-auto-rescan t)
+ '(imenu-auto-rescan-maxout 500000)
+ '(kept-new-versions 5)
+ '(kept-old-versions 5)
+ '(make-backup-file-name-function (quote ignore))
+ '(make-backup-files nil)
+ '(mouse-wheel-follow-mouse nil)
+ '(mouse-wheel-progressive-speed nil)
+ '(mouse-wheel-scroll-amount (quote (15)))
+ '(version-control nil))
+
+(add-to-list 'default-frame-alist '(font . "Liberation Mono-14.0"))
+(set-face-attribute 'default t :font "Liberation Mono-14.0")
+(set-face-attribute 'font-lock-builtin-face nil :foreground "snow3")
+(set-face-attribute 'font-lock-comment-face nil :foreground "gray50")
+(set-face-attribute 'font-lock-constant-face nil :foreground "snow3")
+(set-face-attribute 'font-lock-doc-face nil :foreground "snow3")
+(set-face-attribute 'font-lock-function-name-face nil :foreground "green1")
+(set-face-attribute 'font-lock-keyword-face nil :foreground "yellow")
+(set-face-attribute 'font-lock-string-face nil :foreground "dodgerblue1")
+(set-face-attribute 'font-lock-type-face nil :foreground "magenta")
+(set-face-attribute 'font-lock-variable-name-face nil :foreground "snow3")
+;;(set-face-attribute 'font-lock-builtin-face nil :foreground "#DAB98F")
+
+;; Now set up the font-lock keywords with our custom face
+(font-lock-add-keywords
+ 'c++-mode
+ '(("^[ \t]*#[ \t]*\\(include\\)[ \t]*[<\"]\\([^>\"]*\\)[>\"]"
+    (1 font-lock-preprocessor-face t)   ; Colors the "include" keyword
+    (2 'my-include-filename-face t))))  ; Colors the filename part
+
+;; First, let's create a custom face specifically for include filenames
+(defface my-include-filename-face
+  '((t :foreground "darkorchid"))
+  "Face for filenames in #include statements")
+
+
+;; Set the color for the #include directive itself
+(set-face-attribute 'font-lock-preprocessor-face nil :foreground "darkorchid")
+
+(defun post-load-stuff ()
+  (interactive)
+  (menu-bar-mode -1)
+  (maximize-frame)
+;;  (set-foreground-color "burlywood3")
+  (set-foreground-color "snow3")
+  (set-background-color "#161616")
+  (set-cursor-color "#40FF40")
+)
+(add-hook 'window-setup-hook 'post-load-stuff t)
+(global-unset-key (kbd "M-w"))
+(global-set-key (kbd "M-w") 'kill-ring-save)
